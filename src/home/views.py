870c0b83dbc55fd76e7e64114c37ec1ff5e261d7
Thái Quí <thajquy@gmail.com>
2019-01-27 18:26:05 +0700

    Add template Home to App Home

--- src/home/views.py
+++ src/home/views.py
@@ -5,7 +5,8 @@ from django.http import HttpResponse
 # Create your views here.
 
 def index(request):
-    response = HttpResponse()
-    response.writelines("<h1>Đây là app Home</h1>")
-    response.write('Đây là app home')
-    return response
+    # response = HttpResponse()
+    # response.writelines("<h1>Đây là app Home</h1>")
+    # response.write('Đây là app home')
+    # return response
+    return render(request, 'pages/home.html')
